c28e4ab2eae6954592998d70a1a0be81fb9467cf
tony555t <tonytullah764@gmail.com>
2023-09-08 11:04:47 +0300

    generating seeds

--- database/factories/TodoFactory.php
+++ database/factories/TodoFactory.php
@@ -3,9 +3,18 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Todo;
 
 class TodoFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Todo::class; // Specify the Todo model class
+
     /**
      * Define the model's default state.
      *
@@ -14,7 +23,9 @@ class TodoFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'completed' => false, // You can customize this as needed
         ];
     }
 }
